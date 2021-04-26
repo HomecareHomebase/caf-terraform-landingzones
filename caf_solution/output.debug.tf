6ede51e3617108d96750a956a903f148d1927537
Tanner Watson <tannerwatson@gmail.com>
2021-04-25 20:53:02 -0400

    debug

--- caf_solution/output.debug.tf
+++ caf_solution/output.debug.tf
@@ -1,4 +1,8 @@
-output "debug_caf" {
-  value = module.solution.debug
+output "debug_caf_local_services_roles" {
+  value = module.solution.debug_local_services_roles
   sensitive = true
+}
+
+output "debug_caf_local_combined_objects_managed_identities" {
+  value = module.solution.debug_local_combined_objects_managed_identities
 }
